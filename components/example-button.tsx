3e8e00ef5bac1dccf333b693c782ef711dbc5467
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2026-05-10 17:32:19 +0200

    Adding example button

---
+++ components/example-button.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useTransition } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
+import { getRandomUser } from '@/server-actions/user';
+
+export function ExampleButton() {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const handleClick = async () => {
+    startTransition(async () => {
+      const username = await getRandomUser();
+      if (username) {
+        router.push(`/${username}`);
+      }
+    });
+  };
+
+  return (
+    <Button
+      size="lg"
+      variant="outline"
+      onClick={handleClick}
+      disabled={isPending}
+      className="cursor-pointer"
+    >
+      {isPending ? 'Loading...' : 'See an example'}
+    </Button>
+  );
+}
